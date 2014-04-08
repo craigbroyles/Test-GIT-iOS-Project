35486cd1ef706cb339d38e23d9e9bc87e45bc9cf
craigbroyles <craigbroyles@cbroyles.apple.com>
2014-04-08 15:30:43 -0700

    added test class

---
+++ CABTest.h
@@ -0,0 +1,13 @@
+//
+//  CABTest.h
+//  GIT Test
+//
+//  Created by craigbroyles on 4/8/14.
+//  Copyright (c) 2014 Apple, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CABTest : NSObject
+
+@end
---
+++ CABTest.m
@@ -0,0 +1,13 @@
+//
+//  CABTest.m
+//  GIT Test
+//
+//  Created by craigbroyles on 4/8/14.
+//  Copyright (c) 2014 Apple, Inc. All rights reserved.
+//
+
+#import "CABTest.h"
+
+@implementation CABTest
+
+@end
